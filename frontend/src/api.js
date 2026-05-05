20dc977fdca61a5af5e2398860a94d6b569b5665
krittaphato3 <kritta.pun2549@gmail.com>
2026-05-05 22:52:48 +0700

    fixes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_BASE = import.meta.env.VITE_API_URL || '/api';
+console.log('[api] Base URL:', API_BASE);
 
 export const predict = async (formData) => {
   const response = await axios.post(`${API_BASE}/predict`, formData);
